ed9aeba5412c82debcd1d62a4b56112ac861d302
monir-007 <monir.hossain@northsouth.edu>
2020-07-02 11:53:58 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hello from tech store</h1>
+      <h2 className="text-title">hello from tech store</h2>
+    </>
+  );
+}
+
+export default App;
