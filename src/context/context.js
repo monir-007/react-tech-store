71917ca3e9f19df3398df2755aaec0dd83adacbe
monir-007 <monir.hossain@northsouth.edu>
2020-07-10 02:32:09 +0600

    env development 2

--- src/context/context.js
+++ src/context/context.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import { linkData } from "./linkData";
 import { socialData } from "./socialData";
-import { items } from "./productData";
+//import { items } from "./productData";
+
+import { client } from "./contentful";
 
 const ProductContext = React.createContext();
 
@@ -34,7 +36,14 @@ class ProductProvider extends Component {
 
   componentDidMount() {
     //from contentful items
-    this.setProducts(items);
+    // this.setProducts(items);
+
+    client
+      .getEntries({
+        content_type: "techStoreProduct",
+      })
+      .then((response) => this.setProducts(response.items))
+      .catch(console.error);
   }
 
   //set products
--- src/pages/SingleProducts.js
+++ src/pages/SingleProducts.js
@@ -30,8 +30,8 @@ export default function SingleProducts() {
                 <div className="row">
                   <div className="col-10 mx-auto col-sm-8 col-md-6 my-3">
                     <img
-                      src={`../${image}`}
-                      //src={image}
+                      //src={`../${image}`}
+                      src={image}
                       alt="single product"
                       className="img-fluid"
                     />
